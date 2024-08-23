546d087730f1fb74b4b71f4ec3f4698dbd5ead21
Reinhardbett <bettkiptoo46@gmail.com>
2024-08-23 13:16:30 -0700

    feat: added delete feature and list of objects similar to given type

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,19 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns list of objects of one type of class
+        Else:
+            Returns dictionary of models in storage
+        """
+        if cls is not None:
+            temp = {}
+            for k, v in self.__objects.items():
+                if cls == v.__class__ or cls == v.__class__.name__:
+                    temp[k] = v
+            return (temp)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +55,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if its inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
